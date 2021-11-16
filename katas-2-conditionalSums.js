29b1af367e5f23f064e762a4bf482737ba9cb3f9
hammed-jimoh <hammedgjimoh@yahoo.com>
2021-11-16 22:41:48 +0000

    adding project files

---
+++ katas-2-conditionalSums.js
@@ -0,0 +1,32 @@
+const conditionalSum = function(values, condition) {
+  // Your code here
+  for (let i = 0; i <= values.length; i++){
+    let evenNumbers = [];
+    if (conditionalSum[1] === "even"){
+      i % 2 === 0;
+      evenNumbers.push[i];
+      return evenNumbers;
+    } 
+  console.log(evenNumbers);
+  let oddNumbers = [];
+    if (conditionalSum[1] === "odd"){
+      i % 2 !== 0;
+      oddNumbers.push[i];
+      return oddNumbers;
+    }
+  console.log(oddNumbers);
+  };
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
+
+// set sum to equal zero
+// check the condition
+// loop through the array and fine each element that satisfies the condition
+// add such elements to the sum 
+
+// Do same for the second condition
+// finally output the sum.
