e2b55ac1bbeb0ba067fefb2e14e71c826087cdb3
hammed-jimoh <hammedgjimoh@yahoo.com>
2021-11-17 12:35:13 +0000

    katas assignment 1

--- katas-2-conditionalSums.js
+++ katas-2-conditionalSums.js
@@ -1,32 +1,29 @@
 const conditionalSum = function(values, condition) {
-  // Your code here
-  for (let i = 0; i <= values.length; i++){
-    let evenNumbers = [];
-    if (conditionalSum[1] === "even"){
-      i % 2 === 0;
-      evenNumbers.push[i];
-      return evenNumbers;
-    } 
-  console.log(evenNumbers);
-  let oddNumbers = [];
-    if (conditionalSum[1] === "odd"){
-      i % 2 !== 0;
-      oddNumbers.push[i];
-      return oddNumbers;
+  // First we check the condition.
+  // Then loop through the array and find the values that satisfies the condition.
+  // Add the values that pass the condition into a new variable created, evenSum and oddSum.
+  let evenSum = 0;
+  let oddSum = 0;
+  if (condition === "even"){
+    for (let i = 0; i < values.length; i++){
+      if (values[i] % 2 === 0){
+        evenSum += values[i]
+      };
     }
-  console.log(oddNumbers);
-  };
+    return evenSum;
+  } 
+  else if (condition === "odd"){
+    for (let i = 0; i < values.length; i++){
+      if (values[i] % 2 !== 0){
+        oddSum += values[i]
+      };
+    }
+    return oddSum;
+  }
+  
 };
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
 console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
 console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
 console.log(conditionalSum([], "odd"));
-
-// set sum to equal zero
-// check the condition
-// loop through the array and fine each element that satisfies the condition
-// add such elements to the sum 
-
-// Do same for the second condition
-// finally output the sum.
