f4b839a2f2ae5b485f08904172157b3a3bc1bd5d
hammed-jimoh <hammedgjimoh@yahoo.com>
2021-11-21 23:19:46 +0000

    katas assignment 7

---
+++ katas-7-inTheAirTonight.js
@@ -0,0 +1,34 @@
+const checkAir = function (samples, threshold) {
+  // declare a variable to keep track of how many dirty samples there are in the samples
+  let dirtySamples = 0
+  for (let i = 0; i < samples.length; i++){
+    if (samples[i] === "dirty"){
+      dirtySamples += 1;
+    }
+  }
+  dirtySamples = dirtySamples / 10;
+  
+  //comparing the dirty samples to the threshold
+
+  if (dirtySamples > threshold){
+    return "Polluted";
+  }
+  else {
+    return "Clean";
+  }
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
