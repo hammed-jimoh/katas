2a50bcf9a8160cf4243bccf82b453e959f201a0e
hammed-jimoh <hammedgjimoh@yahoo.com>
2021-12-20 02:47:16 +0000

    katas 5 assignment

--- katas-4-instructorsName.js
+++ katas-4-instructorsName.js
@@ -1,12 +1,12 @@
 const instructorWithLongestName = function(instructors) {
-  // first we create an empty array and put all the instructors names in it
+  // first we create an empty array and put all the instructors names in it and a variable to store the matching instructor's name 
   let names = [];
   let matchedInstructor;
   for (let i = 0; i < instructors.length; i ++){
     names.push(instructors[i]["name"]);
   }
 
-  // now we sort the names in the new array into descending order
+  // now we sort the names in the new NAMES array into descending order
 
   for (let j = 0; j < names.length; j++){
     for (let k = 0; k < names.length - j - 1; k++){
@@ -17,7 +17,7 @@ const instructorWithLongestName = function(instructors) {
     }
   };
 
-  // now we compare the new names array with the instructors array and pick the firsty matching name
+  // now we compare the new NAMES array with the instructors array and pick the firsty matching name, since we arranged it so that the longest name will be first item in the NAMES array
 
   for (let l = 0; l < instructors.length; l++){
     if (instructors[l]["name"] === names[0]){
